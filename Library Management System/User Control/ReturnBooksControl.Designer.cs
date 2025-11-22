20cb9ba34768dd1e41a2893cac21d78901dcbeba
Gabbbyyyyyyyyy <jamesdenoy56@gmail.com>
2025-11-22 20:26:41 +0800

    update the returnbooks

--- Library Management System/User Control/ReturnBooksControl.Designer.cs
+++ Library Management System/User Control/ReturnBooksControl.Designer.cs
@@ -51,11 +51,11 @@
             this.btnReturn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnReturn.FlatAppearance.BorderSize = 0;
             this.btnReturn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReturn.Location = new System.Drawing.Point(277, 278);
             this.btnReturn.Margin = new System.Windows.Forms.Padding(4);
             this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(180, 63);
+            this.btnReturn.Size = new System.Drawing.Size(196, 49);
             this.btnReturn.TabIndex = 1;
             this.btnReturn.Text = "Confirm";
             this.btnReturn.UseVisualStyleBackColor = false;
